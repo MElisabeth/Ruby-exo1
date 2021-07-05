615744e326a0d406c700f014bf71a7d979288955
MElisabeth <86658974+MElisabeth@users.noreply.github.com>
2021-07-05 18:11:55 +0200

    Ex_ruby_1

---
+++ exo_04.rb
@@ -0,0 +1,3 @@
+puts "Salut, ça farte ?"
+
+#marque " exo_04.rb:1: unterminated string meets end of file manque le " à la fin
---
+++ exo_09.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, ton prénom ?"
+prénom = gets.chomp
+puts "Ton nom ?"
+nom = gets.chomp
+puts "Hello," + prénom + " " + nom + "!"
